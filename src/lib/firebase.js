091dc038b755b17e0ec26d3209ce917ff3f5984c
AVINASH KUMAR <91619264+avinash746@users.noreply.github.com>
2024-10-09 01:10:30 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "chatt-app-3173e.firebaseapp.com",
-  projectId: "chatt-app-3173e",
-  storageBucket: "chatt-app-3173e.appspot.com",
-  messagingSenderId: "564166506723",
-  appId: "1:564166506723:web:2d40f207380f8b3cd1edd6",
-  // measurementId: "G-7MFB2XBEE8"
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  // measurementId: ""
 };
 
 export const app = initializeApp(firebaseConfig);
